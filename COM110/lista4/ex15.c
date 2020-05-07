f7a3cd468ebfa3a9d3a5e90f5e6da89a758b028b
Wendel Nogueira <43501645+thehulley@users.noreply.github.com>
2020-05-06 23:25:33 -0300

    Update ex15.c

--- COM110/lista4/ex15.c
+++ COM110/lista4/ex15.c
@@ -6,18 +6,19 @@ int main()
 
 	while(n<2)
 	{
-    printf("Digite um numero:  (Maior ou igual a 2)\n");
+		printf("Digite um numero:  (Maior ou igual a 2)\n");
     scanf("%d",&n);
 	}
-  
-  printf("\n\n-=-=-=-[ Imagem ]-=-=-=-\n");
+	
+	printf("\n\n-=-=-=-[ Imagem ]-=-=-=-\n");
+	
   while(c<n)
 	{
-    for(x = 0; x <= c; x++)
+		for(x = 0; x <= c; x++)
     {
-      printf("#");
-      
-    }  
+			printf("#");
+		}  
+		
     printf("\n");
     c = c+1;
 	}

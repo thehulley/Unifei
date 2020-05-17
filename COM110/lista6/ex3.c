eac6808a6b60a748454510f74b27cd94f042f84a
Wendel Nogueira <43501645+thehulley@users.noreply.github.com>
2020-05-17 17:45:03 -0300

    Update ex3.c

--- COM110/lista6/ex3.c
+++ COM110/lista6/ex3.c
@@ -6,7 +6,7 @@ int main()
   int c, dado, total[6], seq[20];
   char n[2];
 
-  for (c=1 ; c<=6 ; c++)
+  for (c=1 ; c<=6 ; c++)        // "Reset" do valor do vetor total[6]
   {
     total[c] = 0;
   }
@@ -15,7 +15,7 @@ int main()
   {
     dado = 0;
 
-    while ((dado < 1) || (dado > 6))
+    while ((dado < 1) || (dado > 6))        // Proteção contra letras/caracteres especiais/numeros <1 e >6
     {
       printf ("\nQual lado caiu na %dº jogada?\n", c);
       scanf ("%s", n);
@@ -24,19 +24,19 @@ int main()
     }
 
     seq[c] = dado;
-    total[dado] = total[dado] + 1;
+    total[dado] = total[dado] + 1;        // Contador utilizando o vetor para armazenar em cada posição o numero de vezes
   }
 
   printf ("\n\n-=-=-=-=-[ Resultados ]-=-=-=-=-\n\n");
 
-  for (c=1 ; c<=6 ; c++)
+  for (c=1 ; c<=6 ; c++)        // Exibe a quantidade de vezes que o numero "c" caiu
   {
     printf (" - O numero %d caiu %d vez/vezes - \n", c, total[c]);
   }
 
   printf ("\n\n-=-=[ Sequência de jogadas ]=-=-\n\n");
 
-  for (c=1 ; c<=20 ; c++)
+  for (c=1 ; c<=20 ; c++)       // Mostra o valore caido por jogada em ordem
   {
     printf ("|%d| ", seq[c]);
   }

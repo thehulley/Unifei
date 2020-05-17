d6128df3a52c1c2ce827f9e4bb6b1ef62972ead6
Wendel Nogueira <43501645+thehulley@users.noreply.github.com>
2020-05-17 18:05:57 -0300

    Update ex4.c

--- COM110/lista6/ex4.c
+++ COM110/lista6/ex4.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-  int c;
-  char palavra[10];
+  int c, caracteres;
+  char palavra[1000];
 
-  printf ("\nDigite uma palavra com 10 caracteres\n");
-  scanf ("%s", palavra);
+  caracteres = 1001;
+
+  while (caracteres > 1000)       // Proteção contra palavras com mais de 1000 caracteres
+  {
+    printf ("\nDigite uma palavra com 10 caracteres\n");
+    scanf ("%s", palavra);
+
+    caracteres = strlen(palavra);       // Para que o programa não fique muito "limitado" existe a contagem de caracteres da palavra digitada
+
+    if (caracteres > 1000)
+    { 
+      printf ("\nO numero de caracteres da palavra digitada não pode passar de 1000!\n");
+    }
+    else
+    {
+      printf ("\nEssa palavra possui %d letras.\n", caracteres);
+    }
+  }
+
   printf ("\n");
 
-  for (c = 10 ; c >= 0 ; c-- )
-    printf("%c", palavra[c]);
+  for (c=caracteres ; c>=0 ; c--)       // Exibe a palavra, contudo o valor inicial do contador é dado pelo numero de letras da palavra, com isso o programa não trava caso o numero de letras exceda 10.
+  {
+    printf ("%c", palavra[c]);
+  }
+
+  printf ("\n\n");
+
   return 0;
 }

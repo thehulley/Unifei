449f13f1ba64f288d32b06eb3664f1004206c1c0
Wendel Nogueira <43501645+thehulley@users.noreply.github.com>
2020-05-17 21:58:15 -0300

    Update ex5.c

--- COM110/lista6/ex5.c
+++ COM110/lista6/ex5.c
@@ -4,18 +4,24 @@
 
 int main()
 {
-  int c, n, letras, l,a;
-  char palavra[128], alfabeto[27] = "abcdefghijklmnopqrstuvwxyz", p[2];
-
+  int c, i, n, letras, a;
+  char palavra[129], alfabeto[27] = " abcdefghijklmnopqrstuvwxyz", p[4];
 
   printf ("\n\n-=-=-=-=-=-=-=-[ Alfabeto utilizado ]-=-=-=-=-=-=-=-\n\n(a b c d e f g h i j k l m n o p q r s t u v w x y z)\n\n");
 
   printf ("Digite uma palavra\n");
   scanf ("%s", palavra);
 
-  printf ("\nDigite o numero de posições\n");
-  scanf ("%d", &n);
-  a = n;
+  n = 27;
+
+  while ((n < -26) || (n > 26))
+  {
+    printf ("\nDigite o numero de posições        (O numero de posições deve ser maior ou igual -26 e menor ou igual a 26)\n");
+    scanf ("%s", p);
+    
+    n = atoi(p);
+    a = n;
+  }
 
   letras = strlen(palavra);
   printf ("\nEssa palavra tem %d letras", letras);
@@ -24,43 +30,62 @@ int main()
 
   printf ("\n\n-=-=-=-=-=-=-[ Palavra criptografada ]-=-=-=-=-=-=-\n\n");
 
-  for (c=0 ; c < letras ; c++)
+  for (c=letras ; c>0 ; c--)
   {
-    n = a;
-    for (l=0 ; l < 27 ; l++)
+    palavra[c] = palavra[c - 1];
+  }
+
+  palavra[0] = ' ';
+
+  if (n > 0)
+  {
+    for (c=1 ; c<=letras ; c++)
     {
-      if (palavra[c] == alfabeto[l])
+      for (i=1 ; i<=26 ; i++)
       {
-        n = n+l;
-
-        if (c + n > 26)
+        if (palavra[c] == alfabeto[i])
         {
-          n = (c + n)-26;
-          
-          palavra[c] = alfabeto[n];
-
-          printf ("%c", palavra[c]);
-          break;
+          if ((i + n) > 26)
+          {
+            printf ("%c", alfabeto[(i + n) - 26]);
+          }
+          else
+          {
+            printf ("%c", alfabeto[i+n]);
+          }
         }
-        else if ((c + n >= 0) && (c + n <= 26))
-        {
-          palavra[c] = alfabeto[n];
-
-          printf ("%c", palavra[c]);
-          break;
-        }
-        else
-        {
-          n = 26 + n;
-
-          palavra[c] = alfabeto[n];
-
-          printf ("%c", palavra[c]);
-          break;
-        }
-
-        printf ("%d\n",c);
       }
+    }
+  }
+  else if (n < 0)
+  {
+    for (c=1 ; c<=letras ; c++)
+    {
+      for (i=1 ; i<=26 ; i++)
+      {
+        if (palavra[c] == alfabeto[i])
+        {
+          if (i + n < 0)
+          {
+            printf ("%c", alfabeto[26 + (i + n)]);
+          }
+          else if (i + n == 0)
+          {
+            printf ("%c", alfabeto[26]); 
+          }
+          else
+          {
+            printf ("%c", alfabeto[i + n]);
+          }
+        }
+      }
+    }
+  }
+  else
+  {
+    for (c=1 ; c<=letras ; c++)
+    {
+      printf ("%c", palavra[c]);
     }
   }
 

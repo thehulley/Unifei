9a42ffd61a5c9bfa721a979f145f38c9f95d2de7
Wendel Nogueira <43501645+thehulley@users.noreply.github.com>
2020-05-24 01:34:46 -0300

    Update ex2.c

--- COM110/Lista7/ex2.c
+++ COM110/Lista7/ex2.c
@@ -5,77 +5,78 @@
 
 int main()
 {
-  int opcao, c, i, j, k, M[8][8], num;
-  char n[21];
-
-  opcao = 0;        // Inicialização do while
+  int c, i, j, k, M[8][8], num;
+  char n[1];
 
   srand (time(NULL));        // Utilizado em conjunto com o gerador de numeros, inicializa o gerador de numeros
 
-  while ((opcao < 1) || (opcao) > 2)        // Proteção do menu
+  n[0] = 0;
+
+  while ((n[0] < 1) || (n[0] > 2))        // Opcao entre gerar ou não
   {
-    printf ("Escolha uma das opções abaixo\n\n1 - Gerar numeros para cada posição da matriz\n2 - Digitar os numeros da matriz (valor com no maximo 20 caracteres)\n\n");
+    printf ("\n\nGerar matriz ? (1 - sim / 2 - não)\n\n");
     scanf ("%s", n);
 
-    opcao = atoi(n);        // Proteção contra letras
+    n[0] = atoi(n);
   }
 
   system ("clear");
 
-  switch (opcao)
+  if (n[0] == 1)        // Se opcao = 1
   {
-    case 1:
-      while ((j == 0) || (k == 0))
-      {
-        j = (rand() % 9);        // Atribui um numero aleatorio a variavel numero
-        k = (rand() % 9);        // Atribui um numero aleatorio a variavel numero
-      }
+    while ((j == 0) || (k == 0))
+    {
+      j = (rand() % 9);        // Atribui um numero aleatorio a variavel numero
+      k = (rand() % 9);        // Atribui um numero aleatorio a variavel numero
+    }
 
-      printf ("\nNumero de linhas da matriz com numeros = %d.\n", j);
-      printf ("\nNumero de colunas da matriz com numeros = %d.\n", k);
+    printf ("\nNumero de linhas da matriz com numeros = %d.\n", j);
+    printf ("\nNumero de colunas da matriz com numeros = %d.\n", k);
 
-      for (c=0 ; c<8 ; c++)
+    for (c=0 ; c<8 ; c++)
+    {
+      for (i=0 ; i<8 ; i++)
       {
-        for (i=0 ; i<8 ; i++)
+        if ((c < j) && (i < k))
         {
-          if ((c < j) && (i < k))
-          {
-            M[c][i] = (rand() % 100);
-          }
-        }        
-      }
+          M[c][i] = (rand() % 100);
+        }
+      }        
+    }
 
-      printf ("\n\nMatriz gerada\n(X = posição sem valor ou nula)\n\n");
+    printf ("\n\nMatriz gerada\n(X = posição sem valor ou nula)\n\n");
 
-      for (c=0 ; c<8 ; c++)
+    for (c=0 ; c<8 ; c++)
+    {
+      for (i=0 ; i<8 ; i++)
       {
-        for (i=0 ; i<8 ; i++)
+        if ((c < j) && (i < k))
         {
-          if ((c < j) && (i < k))
-          {
-            printf ("[%d]", M[c][i]);
-          }
-          else
-          {
-            printf ("[X]");
-          }
-        }     
-        printf ("\n");   
-      }
+          printf ("[%d]", M[c][i]);
+        }
+        else
+        {
+          printf ("[X]");
+        }
+      }     
+      printf ("\n");   
+    }
 
-      if (j == k)
-      {
-        printf ("\n\nA matriz é simetrica\n\n");
-      }
-      else
-      {
-        printf ("\n\nA matriz não é simetrica\n\n");
-      }
-    break;
-
-    case 2:
-    break;  
+    if (j == k)
+    {
+      printf ("\n\nA matriz é simetrica\n\n");
+    }
+    else
+    {
+      printf ("\n\nA matriz não é simetrica\n\n");
+    }
   }
+  else
+  {
+    printf ("\n\nA opção 2 foi selecionada, portando fechando o programa\n\n");
+  }
+
   printf ("\n\nPrograma Finalizado\n\n");
+
   return 0;
 }
